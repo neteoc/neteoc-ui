0672ae161d20e04eb4785221ac39f6c42fa11f40
Kerry Hatcher <kwhatcher@gmail.com>
2016-08-23 16:52:07 -0400

    inital import

---
+++ index.js
@@ -0,0 +1,20 @@
+
+require("adminjs-bootstrap-webpack!./bootstrap.config.js");
+require("adminjs-datatables-webpack!./datatables.config.js");
+
+
+
+import { HawtioCore , hawtioPluginLoader } from 'adminjs-core';
+
+import {HawtioMainNav} from 'adminjs-core-navigation';
+
+
+
+require("angular-http-auth");
+require("./user");
+
+import {} from './flare';
+
+
+
+//test me
