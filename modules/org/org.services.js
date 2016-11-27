a2c1cdb8e9debec5df2f4ee22a78c935a920d81e
Kerry Hatcher <kwhatcher@gmail.com>
2016-11-26 20:39:20 -0500

    Activated the Org Module
    Activaged the Org module and added relevant stuff to mockapi

--- modules/org/org.services.js
+++ modules/org/org.services.js
@@ -3,27 +3,27 @@
 
 module.exports = function(parentModule) {
 
-  
+
   parentModule._module
-      .factory('Org', function ($resource) {
-          return $resource('/api/org/:id', { id: '@_id' },
+      .factory('Org', ['$resource', '$appEnvironment', function ($resource, $appEnvironment) {
+          return $resource($appEnvironment.config.apiUrl + 'org/:id', { id: '@_id' },
               {
                   'update': {
                       method:'PUT'
                   }
               });
-      });
+      }]);
 
 
     parentModule._module
-        .factory('Invite', function ($resource) {
-            return $resource('/api/org/:orgid/invite/:inviteid', { orgid: '@orgid', inviteid:'@inviteid' },
+        .factory('Invite', ['$resource', '$appEnvironment', function ($resource, $appEnvironment) {
+            return $resource($appEnvironment.config.apiUrl + 'org/:orgid/invite/:inviteid', { orgid: '@orgid', inviteid:'@inviteid' },
                 {
                     'update': {
                        method:'PUT'
                     }
                 });
-        });
+        }]);
 
 
 
