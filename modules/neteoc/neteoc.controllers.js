d73d9f87b2a4af1a3685a4334214527f082c18dc
Kerry Hatcher <kwhatcher@gmail.com>
2016-12-23 10:42:34 -0500

    only send to profile after login

--- modules/neteoc/neteoc.controllers.js
+++ modules/neteoc/neteoc.controllers.js
@@ -2,17 +2,21 @@
 
 module.exports = function(parentModule) {
 
-    parentModule.HomePageController = parentModule._module.controller(parentModule.pluginName + '.HomePageController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+    parentModule.HomePageController = parentModule._module.controller(parentModule.pluginName + '.HomePageController', ['$scope', '$http', '$location', 'authService', function($scope, $http, $location, authService) {
         let vm = this;
         let title = "Home";
 
-        if($location.path() == "/"){
-          console.log("default path!");
+        if(authService.getAuthState()){
+            if($location.path() == "/"){
+                console.log("default path!");
 
-          console.log($location.path());
-          $location.search('main-tab=User&sub-tab=User-profile')
-          $location.path('/ui/user/profile')
+                console.log($location.path());
+                $location.search('main-tab=User&sub-tab=User-profile')
+                $location.path('/ui/user/profile')
+            }
         }
+
+
 
 
 
