b051f49fba5dfd5e30abe6630d41e2de912d0b48
EricWehrly <eric@disruptiontheory.com>
2016-12-30 21:42:21 -0500

    Organization now integrated with the rest of app

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
 import Profile from './profile/profile';
+import Organization from './organization/organization';
 
 let componentModule = angular.module('app.components', [
   Home,
   About,
-  Profile
+  Profile,
+  Organization
 ])
 
 .name;
--- client/app/components/organization/organization.js
+++ client/app/components/organization/organization.js
@@ -3,8 +3,22 @@ import uiRouter from 'angular-ui-router';
 import organizationComponent from './organization.component';
 
 let organizationModule = angular.module('organization', [
-  uiRouter
+  uiRouter,  
+  'navbar'
 ])
+
+.config(($stateProvider) => {
+  "ngInject";
+  $stateProvider
+    .state('organization', {
+      url: '/organization',
+      component: 'organization'
+    });
+})
+
+.run(['Menu', function(Menu){
+  Menu.addToMainMenu({ display: "Organization", url: "/organization", requireLogin: true })
+}])
 
 .component('organization', organizationComponent)
 
