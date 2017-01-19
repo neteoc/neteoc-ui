d7d8a8be9b3f5706c53e2522242fc6ad0d98fdc2
EricWehrly <eric@disruptiontheory.com>
2017-01-18 21:03:03 -0500

    More lists stuff

--- client/app/components/flare/lists/lists.controller.js
+++ client/app/components/flare/lists/lists.controller.js
@@ -1,6 +1,29 @@
 class ListsController {
   constructor() {
     this.name = 'lists';
+
+    this.organizations = this.getOrgs();
+    $(document).ready(function() {
+        $('#orglisttable').DataTable();
+    } );
+  }
+
+  // TODO: Get from API
+  getOrgs = () => {
+
+    let orgs = [
+      {
+        _id: 1,
+        name: "u",
+        description: "none of u bidness"
+      },
+      {
+        _id: 2,
+        name: "me",
+        description: "likes puppies"
+      }
+    ];
+    return orgs;
   }
 }
 
