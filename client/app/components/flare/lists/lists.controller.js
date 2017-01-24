ac95b3bbaae322061b2ce0071c9fa380ceff1ffc
EricWehrly <eric@disruptiontheory.com>
2017-01-23 21:55:08 -0500

    Users can now make lists and see associated lists

--- client/app/components/flare/lists/lists.controller.js
+++ client/app/components/flare/lists/lists.controller.js
@@ -3,14 +3,36 @@ class ListsController {
     this.name = 'lists';
     this.$http = $http;
 
+    this.newList = {};
+
+  // TODO: This ain't right. Auth service or something, right?
+  var authToken = localStorage.getItem('id_token');
+  $http.defaults.headers.common.Authorization = 'bearer ' + authToken;
+    this.userId = localStorage.getItem('neteoc_id');
+
     this.getAvailableLists();
     this.getOrganizations();
+
+    this.listsGrid = {
+      data: '$ctrl.lists',
+      columnDefs: [{
+        name: 'Name',
+        field: 'name'
+      }, {
+        name: 'Description',
+        field: 'description'
+      }, {
+        name: ' ',
+        // TODO: If owner, have button to add users to list ...
+        cellTemplate: '<div><button ng-click="grid.appScope.organizationDetails(row.entity.id)">Details</button></div>'
+      }]
+    };
   }
 
   getAvailableLists = () => {
 
     let vm = this;
-    this.$http.get('https://mockapi.neteoc.com/lists/').then(function(response) {
+    this.$http.get('http://54.172.225.43:55142/users/' + this.userId + "/flaregroups").then(function(response) {
 
       angular.extend(vm, {
         lists: response.data
@@ -21,11 +43,24 @@ class ListsController {
   getOrganizations = () => {
 
     let vm = this;
-    this.$http.get('https://mockapi.neteoc.com/organizations/').then(function(response) {
+    this.$http.get('http://54.172.225.43:54362/users/' + this.userId + '/organizations/').then(function(response) {
 
       angular.extend(vm, {
         organizations: response.data
       });
+    });
+  }
+
+  createList = () => {
+
+    this.newList.ownerId = this.userId;
+    this.newList.organizationId = this.selectedOrganization.id;
+
+    let vm = this;
+    this.$http.post('http://54.172.225.43:55142/flaregroups', this.newList).then(function(response) {
+
+        console.log(response.data);
+        vm.getAvailableLists();
     });
   }
 }
