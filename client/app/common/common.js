9963fd43fba7e28fcbfe7bf88acab19a64da87c1
EricWehrly <eric@disruptiontheory.com>
2017-02-02 22:37:49 -0500

    Added exif service Tried to add as npm package ... that was so much harder than it needed to be.

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -3,12 +3,14 @@ import Navbar from './navbar/navbar';
 import Footernav from './footernav/footernav'
 import Hero from './hero/hero';
 import User from './user/user';
+import Exif from './exif/exif';
 
 let commonModule = angular.module('app.common', [
   Navbar,
   Footernav,
   Hero,
-  User
+  User,
+  Exif
 ])
 
 .name;
