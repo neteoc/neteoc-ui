9721ae778d17ec34525e86fb4894d51878308f19
Kerry Hatcher <kwhatcher@gmail.com>
2017-09-29 18:11:29 -0400

    maps work but don't

--- src/app/geo/geo.component.ts
+++ src/app/geo/geo.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component
+  , OnInit
+  , ViewChild } from '@angular/core';
 import { LeafletMapComponent } from '../leaflet-map/leaflet-map.component';
 
 @Component({
@@ -7,10 +9,19 @@ import { LeafletMapComponent } from '../leaflet-map/leaflet-map.component';
   styleUrls: ['./geo.component.css']
 })
 export class GeoComponent implements OnInit {
-
+  @ViewChild(LeafletMapComponent) _leafletMap: LeafletMapComponent;
+  pin = {};
+  mapParams = {};
+  tileData = {};
   constructor() { }
 
   ngOnInit() {
+    this.mapParams =  {zoom: 13, center: [51.505, -0.09]};
+    this.tileData = {
+      url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution: 'Ma',
+    }
+    this._leafletMap.initialize(this.mapParams, this.tileData );
   }
 
 }
